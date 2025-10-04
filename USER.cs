f8f27b298608715659ac456553833be74dd5ab81
Samir <Samirpolozen@gmail.com>
2025-10-04 12:16:36 +0200

    La till en konstruktor för User klassen

---
+++ USER.cs
@@ -0,0 +1,15 @@
+namespace app; // skapar namespace app som kommer att kopplas till program.cs
+
+class User // skapar en User klass som innehåller metoder för olika funktioner 
+{
+      public string Email;
+      public string Password;
+
+      //Skapar en konstruktor för klass User
+      // När vi skapar ett User objekt skickar vi parametrarna email och password 
+      public User(string email, string password)
+      {
+            Email = email; // Tilldelar värdet email
+            Password = password; // Tilldelar password värdet
+      }
+}
