2d2cd2d22ecf302225d5cdf7cb47354864cc41f6
Samir <Samirpolozen@gmail.com>
2025-10-04 12:26:38 +0200

    La till en meny förtillfället utan någon använding, endast val.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using app; // Kopplar User.cs till Main
+
+
+// Skapar en meny
+// Endast kunna registerar, logga in och avsluta loopen
+bool running = true;
+
+while (running)
+{
+      Console.WriteLine("----HUVUD MENY---");
+      Console.WriteLine("-----------------");
+      Console.WriteLine("1. Registrera");
+      Console.WriteLine("2. Logga in");
+      Console.WriteLine("3. Avsluta");
+      string input = Console.ReadLine();
+      
+      // Kommer använda mig av switch input för tycker det är smidigast 
+      switch (input)
+      {
+            case "1":
+
+                  break;
+            case "2":
+
+                  {
+
+                  }
+                  break;
+            case "3":
+                  running = false;
+                  break;
+
+
+      }
+      
+
+}
